fa413cfae8495988d6fe1683befdc07aa99ca3b1
bonbonpa <darksphin@gmail.com>
2017-02-11 20:59:39 +0700

    9. Edit destructuring key

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import App ,{ AppDecription } from './components/app'; //{} is Destructuring
 
 const randomObject = { Maha : '10'  , Art : '20'};
-const Maha = randomObject.Maha; //access to value
+const { Maha }  = randomObject; //access to Object value 
+//out 10
 
 console.log(AppDecription);
 console.log(Maha);
