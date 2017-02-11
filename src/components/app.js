206b6bc12a0b9ca4b8c0a35e060ff86be124603c
bonbonpa <darksphin@gmail.com>
2017-02-11 20:32:16 +0700

    4. Typing console.log and ru n test npm start

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 //() => fat arrow function set up by es6 , function(){}
 const App = () => ( 
-  <div> 
-  HI 
+  <div>
+    HI 
   </div>
 );
+
+console.log(App());
 
 export default App;
