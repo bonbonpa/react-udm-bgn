73982d4a684d24d9c6add7bc47e54be739fd330a
bonbonpa <darksphin@gmail.com>
2017-02-11 23:26:11 +0700

    19. Add clicked : PropTypes

--- src/components/button.js
+++ src/components/button.js
@@ -3,12 +3,13 @@ import React, { PropTypes } from 'react';
 const Button = (props) => { 
    // console.log('props',props);
    return (
-    <button>{props.name}</button> //props for access 
+    <button onClick={props.clicked}>{props.name}</button> //props for access 
    );   
 };
 
 Button.PropTypes = {
     name : PropTypes.string,
+    clicked : PropTypes.func,
 };  
 
 export default Button
