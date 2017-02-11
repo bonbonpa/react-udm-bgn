c220c3361a59ec3048d6556cfca8cfb120310661
bonbonpa <darksphin@gmail.com>
2017-02-11 20:58:17 +0700

    8. Access To Object

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App ,{ AppDecription } from './components/app'; //{} is Destructuring
 
+const randomObject = { Maha : '10'  , Art : '20'};
+const Maha = randomObject.Maha; //access to value
+
 console.log(AppDecription);
+console.log(Maha);
 
 ReactDOM.render(<App/> , document.querySelector('.container'));
 
